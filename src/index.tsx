3994da47f0030d291921a033d5f7a8907e94fd65
TUCCHI1 <tutikikin1@gmail.com>
2024-09-14 13:32:12 +0900

    feat: Implement server-side rendering for improved initial load speed and SEO benefits.

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import { Hono } from 'hono'
 import { renderer } from './renderer'
+import { renderToString } from 'react-dom/server'
 
 const app = new Hono()
 
@@ -11,6 +12,26 @@ app.get("/api/clock", (c) => {
   })
 })
 
-app.get("*", (c) => ww)
+app.get("*", (c) => {
+  return c.html(
+    renderToString(
+      <html>
+        <head>
+          <meta charSet='utf-8' />
+          <meta content="width=device-width, initial-scale=1" name="viewport" />
+          <link rel="stylesheet" href="https://cdn.simplecss.org/simple.min.css" />
+          {import.meta.env.PROD ? (
+            <script type="module" src="/static/client.js"></script>
+          ) : (
+            <script type="module" src="/src/client.tsx"></script>
+          )}
+        </head>
+        <body>
+          <div id="root"></div>
+        </body>
+      </html>
+    )
+  )
+})
 
 export default app
